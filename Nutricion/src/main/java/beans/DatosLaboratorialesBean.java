342325775b3cb21b0bb08dc3e6fb8f103bb5e278
Oscar Ruffinelli <ozkrpy@gmail.com>
2016-10-07 11:12:43 -0300

    Renders para pagina nuevoPaciente

--- Nutricion/src/main/java/beans/DatosLaboratorialesBean.java
+++ Nutricion/src/main/java/beans/DatosLaboratorialesBean.java
@@ -33,35 +33,69 @@ public class DatosLaboratorialesBean {
     public DatosLaboratorialesBean() {
     }
 
+    public String laboratorioProteinuria(float param) {
+        double MIN = 0.8;
+        double MAX = 1.2;
+        /*DEFINIR PARAMETROS POR EL MOMENTO RETORNA NULL*/
+        return null;
+    }
+
+    public String laboratorioHdl(float param) {
+        double MIN = 40;
+        double MAX = 200;
+        /* REVISAR */
+        return calcular(param, MIN, MAX);
+    }
+
+    public String laboratorioPotasio(float param) {
+        double MIN = 40;
+        double MAX = 80;
+        /* DEFINIDO EN LOS 2 DOCUMENTOS*/
+        return calcular(param, MIN, MAX);
+    }
+
+    public String laboratorioGlobulosBlancos(float param) {
+        double MIN = 0.8;
+        double MAX = 1.2;
+        /*DEFINIR*/
+        return calcular(param, MIN, MAX);
+    }
+
+    public String laboratorioHba(float param) {
+        double MIN = 0.8;
+        double MAX = 1.2;
+        /*DEFINIR*/
+        return calcular(param, MIN, MAX);
+    }
+
     public String laboratorioGlicemiaBasal(float param) {
         double MIN = 70;
         double MAX = 110;
-        
+
         return calcular(param, MIN, MAX);
     }
 
     public String laboratorioCreatinina(float param) {
         double MIN = 0.8;
         double MAX = 1.2;
-        
+
         return calcular(param, MIN, MAX);
     }
-    
+
     public String laboratorioProteinasTotales(float param) {
         double MIN = 6;
         double MAX = 8.30;
 
         return calcular(param, MIN, MAX);
     }
-    
+
     public String laboratorioUrea(float param) {
         double MIN = 10;
         double MAX = 50;
 
         return calcular(param, MIN, MAX);
     }
-    
-    
+
     public String laboratorioAcidoUrico(float param, String sexo) {
         double MAX_MUJER = 6;
         double MAX_HOMBRE = 7;
@@ -72,74 +106,52 @@ public class DatosLaboratorialesBean {
             } else {
                 return null;
             }
-        } else if (sexo.equals("F")){
+        } else if (sexo.equals("F")) {
             if (param > 0 && param <= MAX_MUJER) {
                 return RETORNO_OK;
             } else {
                 return null;
             }
         }
-        
+
         return null;
     }
-    
-    
-    public String laboratorioProteinuria(float param) {
-        double MIN = 0.8;
-        double MAX = 1.2;
-        /*DEFINIR PARAMETROS POR EL MOMENTO NULL*/
-        return null;
-    }
-    
-    
+
     public String laboratorioAlbumina(float param) {
         double MIN = 3.5;
         double MAX = 5.5;
 
         return calcular(param, MIN, MAX);
     }
-    
-    
+
     public String laboratorioTriglicerios(float param) {
         double MIN = 1;
         double MAX = 159;
 
         return calcular(param, MIN, MAX);
     }
-    
-    
+
     public String laboratorioColesterolTotal(float param) {
         double MIN = 1;
         double MAX = 199;
 
         return calcular(param, MIN, MAX);
     }
-    
-    
+
     public String laboratorioLdl(float param) {
         double MIN = 1;
         double MAX = 130;
 
         return calcular(param, MIN, MAX);
     }
-    
-    
-    public String laboratorioHdl(float param) {
-        double MIN = 40;
-        double MAX = 200;
-        /* REVISAR */
-        return calcular(param, MIN, MAX);
-    }
-    
-    
+
     public String laboratorioGlucosa(float param) {
         double MIN = 125;
         double MAX = 135;
 
         return calcular(param, MIN, MAX);
     }
-    
-    
+
     public String laboratorioHb(float param, String sexo) {
         double MIN_HOMBRE = 14;
         double MAX_HOMBRE = 16;
@@ -148,13 +160,12 @@ public class DatosLaboratorialesBean {
 
         if (sexo.equals("M")) {
             return calcular(param, MIN_HOMBRE, MAX_HOMBRE);
-        } else if (sexo.equals("F")){
+        } else if (sexo.equals("F")) {
             return calcular(param, MIN_MUJER, MAX_MUJER);
-        }        
+        }
         return null;
     }
-    
-    
+
     public String laboratorioHematocrito(float param, String sexo) {
         double MIN_HOMBRE = 40;
         double MAX_HOMBRE = 52;
@@ -163,13 +174,12 @@ public class DatosLaboratorialesBean {
 
         if (sexo.equals("M")) {
             return calcular(param, MIN_HOMBRE, MAX_HOMBRE);
-        } else if (sexo.equals("F")){
+        } else if (sexo.equals("F")) {
             return calcular(param, MIN_MUJER, MAX_MUJER);
-        }        
+        }
         return null;
     }
-    
-    
+
     public String laboratorioGlobulosRojos(float param, String sexo) {
         double MIN_HOMBRE = 4.6;
         double MAX_HOMBRE = 6.1;
@@ -178,54 +188,28 @@ public class DatosLaboratorialesBean {
 
         if (sexo.equals("M")) {
             return calcular(param, MIN_HOMBRE, MAX_HOMBRE);
-        } else if (sexo.equals("F")){
+        } else if (sexo.equals("F")) {
             return calcular(param, MIN_MUJER, MAX_MUJER);
-        }        
+        }
         return null;
     }
-    
-    
-    public String laboratorioPotasio(float param) {
-        double MIN = 40;
-        double MAX = 80;
-        /* DEFINIDO EN LOS 2 DOCUMENTOS*/
-        return calcular(param, MIN, MAX);
-    }
-    
-    
+
     public String laboratorioCi(float param) {
         double MIN = 97;
         double MAX = 107;
 
         return calcular(param, MIN, MAX);
     }
-    
-    
+
     public String laboratorioNa(float param) {
         double MIN = 135;
         double MAX = 145;
 
         return calcular(param, MIN, MAX);
     }
-    
-    
-    public String laboratorioGlobulosBlancos(float param) {
-        double MIN = 0.8;
-        double MAX = 1.2;
-        /*DEFINIR*/
-        return calcular(param, MIN, MAX);
-    }
-    
-    
-    public String laboratorioHba(float param) {
-        double MIN = 0.8;
-        double MAX = 1.2;
-        /*DEFINIR*/
-        return calcular(param, MIN, MAX);
-    }
-    
+
     private String calcular(float param, double min, double max) {
-        
+
         if (param > 0 && param < min) {
             return RETORNO_BELOW;
         } else if (param >= min && param <= max) {
